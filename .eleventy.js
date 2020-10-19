a532f9342fa053e2ea1d9dfd0dd487f78ae18ac0
Glitch (cubic-pouncing-diascia) <none>
2020-10-19 01:20:02 +0000

    Rewound to commit b9ce8b933a61dfac97f0d19d9914030e7684d8a4 + 0 edits

--- .eleventy.js
+++ .eleventy.js
@@ -1,50 +1,25 @@
-const pluginRss = require('@11ty/eleventy-plugin-rss')
-const markdownIt = require('markdown-it')
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setTemplateFormats([
+    // Templates:
+    'html',
+    'njk',
+    'md',
+    // Static Assets:
+    'css',
+    'jpeg',
+    'jpg',
+    'png',
+    'svg',
+    'woff',
+    'woff2',
+  ]);
+  eleventyConfig.addPassthroughCopy('static');
 
-
-module.exports = function (config) {
-    // Plugins
-    config.addPlugin(pluginRss)
-
-
-    // Asset Watch Targets
-    config.addWatchTarget('./src/assets')
-
-    // Markdown
-    config.setLibrary(
-        'md',
-        markdownIt({
-            html: true,
-            breaks: true,
-            linkify: true,
-            typographer: true
-        })
-    )
-
-    // Layouts
-    config.addLayoutAlias('base', 'base.njk')
-    config.addLayoutAlias('post', 'post.njk')
-
-    // Pass-through files
-    config.addPassthroughCopy('src/robots.txt')
-    config.addPassthroughCopy('src/site.webmanifest')
-    config.addPassthroughCopy('src/assets/images')
-    config.addPassthroughCopy('src/assets/fonts')
-
-    // Deep-Merge
-    config.setDataDeepMerge(true)
-
-    // Base Config
-    return {
-        dir: {
-            input: 'src',
-            output: 'dist',
-            includes: 'includes',
-            layouts: 'layouts',
-            data: 'data'
-        },
-        templateFormats: ['njk', 'md', '11ty.js'],
-        htmlTemplateEngine: 'njk',
-        markdownTemplateEngine: 'njk'
-    }
-}
+  return {
+    dir: {
+      input: '_template',
+      includes: '../_includes',
+      output: '_output',
+    },
+  };
+};
