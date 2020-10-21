bd9cc3efd851e1bee6b824a1f82cf5099830674b
Glitch (cubic-pouncing-diascia) <none>
2020-10-21 06:00:55 +0000

    🐙🎟 Checkpoint ./posts/first-blog-post.md:617503/180 ./blog.njk:617503/4765 ./package.json:617503/1296 ./.eleventy.js:617503/2815

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,40 @@
-module.exports = function (eleventyConfig) {
+const pluginNavigation = require("@11ty/eleventy-navigation");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginNavigation);
+
+  eleventyConfig.addFilter("head", (array, n) => {
+    if (n < 0) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
+  eleventyConfig.addFilter("min", (...numbers) => {
+    return Math.min.apply(null, numbers);
+  });
+
   eleventyConfig.setTemplateFormats([
     // Templates:
-    'html',
-    'njk',
-    'md',
+    "html",
+    "njk",
+    "md",
     // Static Assets:
-    'css',
-    'jpeg',
-    'jpg',
-    'png',
-    'svg',
-    'woff',
-    'woff2',
+    "css",
+    "jpeg",
+    "jpg",
+    "png",
+    "svg",
+    "woff",
+    "woff2"
   ]);
-  eleventyConfig.addPassthroughCopy('static');
+  eleventyConfig.addPassthroughCopy("static");
 
   return {
     dir: {
-      includes: './_includes',
-      output: '_output',
-    },
+      includes: "./_includes",
+      output: "_output"
+    }
   };
 };
--- posts/first-blog-post.md
+++ posts/first-blog-post.md
@@ -1,9 +1,9 @@
 ---
-title: This is my first post.
-description: This is a post on My Blog about agile frameworks.
-date: 2018-05-01
+title: This is my first post on Glitch!
+description: 11ty makes it really easy to create a blog
+date: 2020-10-10
 tags:
-  - another-tag
+  - tagged
 layout: layouts/post.njk
 ---
 
