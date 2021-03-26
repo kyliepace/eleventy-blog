2c2d2c2453d3c3d758e64ae40b647f98af8a9229
Glitch (cubic-pouncing-diascia) <none>
2021-03-26 02:06:01 +0000

    🏤🌂 Checkpoint ./src/_includes/seo/seo.json:1062301/262 ./package.json:1062301/14 ./.eleventy.js:1062301/517

--- .eleventy.js
+++ .eleventy.js
@@ -18,7 +18,12 @@ module.exports = function(eleventyConfig) {
   ]);
   eleventyConfig.addPassthroughCopy("static");
   
-  eleventyConfig.addPlugin(pluginSEO, require("./src/_includes/seo/seo.json"));
+  eleventyConfig.addPlugin(pluginSEO, {
+    "title": "Hello Eleventy!",
+    "description": "Build your own Eleventy blog on Glitch.",
+    "url":`https://.glitch.me`,
+    "image": "https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2Fhello-eleventy-social.png?v=1616712747908"
+  });
 
   eleventyConfig.addFilter("htmlDateString", dateObj => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
